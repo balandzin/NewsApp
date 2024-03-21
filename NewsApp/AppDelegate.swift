ee95daad63fdaa47016b3565005eef212265ccb8
balandzin <balandzin2602@gmail.com>
2024-03-21 20:42:25 +0300

    initial commit

---
+++ NewsApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  NewsApp
+//
+//  Created by Антон Баландин on 21.03.24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
